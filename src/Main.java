276de1945336b53fa263b69466cbc91b785ec77b
Maxim Lamanovskiy <maxim.lamanovsky@7bits.it>
2019-10-02 10:45:36 +0600

    Add counter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!\n\n");
+
+        System.out.println("Start counter");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + 1);
+        }
+        System.out.println("Stop counter");
     }
 }
